999eec2f853d77a842000928e49c7efc70232d6a
CyborgDitz <sebban.lol@gmail.com>
2024-09-16 10:16:08 +0200

    Gitignored

--- Helloworld/obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
+++ Helloworld/obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11c2deff89fc90447ebdb21b9d26b1d0c17c77b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
